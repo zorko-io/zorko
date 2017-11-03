528b6c1b65cde21c1af14a853aa21b3d76e0bdc6
nesterone <igornester@gmail.com>
2017-11-03 21:57:42 +0200

    - provide simple api server

---
+++ server/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'extends': 'standard',
+  'rules': {
+    'semi': 2,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+};
