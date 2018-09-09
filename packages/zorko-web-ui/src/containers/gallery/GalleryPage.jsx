92bf9d5f924f5f6d75ebf42dc39df74833f563fa
Igor Nesterenko <igornester@gmail.com>
2018-09-09 17:48:12 +0300

    - provide pagination within gallery component

--- packages/zorko-web-ui/src/containers/gallery/GalleryPage.jsx
+++ packages/zorko-web-ui/src/containers/gallery/GalleryPage.jsx
@@ -1,59 +1,61 @@
 import React, { Component, Fragment } from 'react'
 import PreviewGrid from '../preview/PreviewGrid'
 import connect from 'react-redux/es/connect/connect'
-import PropTypes from 'prop-types'
-import GalleryIntroSection from './GalleryIntroSection'
-import * as previews from "../../selector/previews";
+import GalleryIntroSection from '../intro/IntroSection'
 import {Link} from "react-router-dom";
 import {bindActionCreators} from "redux";
 import {specLookupsRequest} from "../../action";
 
+const getPageId = (props) => (Number(props.match.params.pageId));
+
 class GalleryPage extends Component {
 
   get pageId () {
-    return Number(this.props.match.params.pageId);
+    return getPageId(this.props);
   }
 
   componentDidMount () {
+    this.requestPreviews(this.pageId);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const prevPageId = this.pageId;
+    const nextPageId = getPageId(nextProps);
+    if (prevPageId !== nextPageId) {
+      this.requestPreviews(nextPageId);
+    }
+  }
+
+  requestPreviews = (pageId) => {
     let limit = 10;
     let offset = 1;
 
-    if (this.pageId) {
-      offset = this.pageId * 9;
+    if (pageId) {
+      offset = pageId * 9;
       limit =  9;
     }
     this.props.requestPreviews({limit, offset})
-  }
+  };
 
   render() {
-    const { previousUrl, nextUrl } = this.props;
+    const { match } = this.props;
 
     return (
       <Fragment>
         <GalleryIntroSection />
         <PreviewGrid />
-        {previousUrl && (
-          <Link to={previousUrl}>
+        {this.pageId && (
+          <Link to={`${match.path.replace(':pageId', this.pageId - 1)}`}>
             <button className="button is-disabled">Prev</button>
           </Link>
         )}
-        <Link to={nextUrl}>
+        <Link to={`${match.path.replace(':pageId', this.pageId + 1)}`}>
           <button className="button is-primary">Next</button>
         </Link>
       </Fragment>
     )
   }
 }
-
-GalleryPage.propTypes = {
-  previousUrl: PropTypes.string,
-  nextUrl: PropTypes.string
-};
-
-const mapStateToProps = (state) => ({
-  previousUrl: previews.getPreviousUrl(state),
-  nextUrl: previews.getNextUrl(state),
-});
 
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(
@@ -64,5 +66,5 @@ const mapDispatchToProps = (dispatch) =>
   )
 
 export default connect(
-  mapStateToProps, mapDispatchToProps
+  null, mapDispatchToProps
 )(GalleryPage)
