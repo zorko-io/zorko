073c2af411f3cf47ea6f11ce050a80275d312d71
Igor Nesterenko <igornester@gmail.com>
2018-10-25 10:52:37 +0300

    - add react modal to the project (without styling)

--- packages/zorko-web-ui/src/App.jsx
+++ packages/zorko-web-ui/src/App.jsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import GalleryPage from './containers/gallery/GalleryPage'
 import Redirect from 'react-router/es/Redirect'
 import ViewerPage from './containers/viewer/ViewerPage'
+import NewSpecWizardDialog from './containers/new-spec-wizard/NewSpecWizardDialog'
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact={true} path={'/'} component={GalleryPage}/>
-        <Route exact={true} path={'/gallery/0'} render={() => <Redirect to={'/'}/>}/>
-        <Route path={'/gallery/:pageId'} component={GalleryPage}/>
-        <Route path={'/specs/:specId'} component={ViewerPage} />
-      </Switch>
+      <Fragment>
+        <NewSpecWizardDialog/>
+        <Switch>
+          <Route exact={true} path={'/'} component={GalleryPage}/>
+          <Route exact={true} path={'/gallery/0'} render={() => <Redirect to={'/'}/>}/>
+          <Route path={'/gallery/:pageId'} component={GalleryPage}/>
+          <Route path={'/specs/:specId'} component={ViewerPage} />
+        </Switch>
+      </Fragment>
     )
   }
 }
--- packages/zorko-web-ui/src/index.js
+++ packages/zorko-web-ui/src/index.js
@@ -7,6 +7,7 @@ import createAppStore from './store'
 import {bootstrapStart} from "./action/bootstrap";
 import App from "./App";
 import {HashRouter} from "react-router-dom";
+import Modal from 'react-modal'
 
 const store = createAppStore();
 
@@ -14,12 +15,16 @@ store.dispatch(bootstrapStart({
   baseUrl: process.env.REACT_APP_ZORKO_SERVER_BASE_URL
 }));
 
+const APP_ELEMENT_ID = 'root';
+
+Modal.setAppElement(`#${APP_ELEMENT_ID}`)
+
 ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
       <App />
     </HashRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById(APP_ELEMENT_ID)
 )
 registerServiceWorker()
