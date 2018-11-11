e6a878667834a2f191653a52500f92a204273687
Igor Nesterenko <igornester@gmail.com>
2018-11-11 20:02:14 +0200

    - send preview from client however it's not adequate ...

--- packages/zorko-web-ui/src/api/specs.js
+++ packages/zorko-web-ui/src/api/specs.js
@@ -1,5 +1,39 @@
 import axios from 'axios/index'
 import { API_DEFAULT_PAGGINATION_OPTIONS } from '../constants'
+import { compile } from 'vega-lite'
+import * as vega from "vega"
+
+
+// TODO: replace with server-side
+function modifyDataSourceLocation(spec) {
+  if (spec.data && spec.data.url) {
+    let url = spec.data.url
+
+    spec.data.url = `https://raw.githubusercontent.com/vega/vega-datasets/gh-pages/${url}`
+  }
+
+  if (spec.layer && spec.layer.length > 0) {
+    spec.layer = spec.layer.map(layer => {
+      let result = { ...layer }
+      if (layer.data && layer.data.url) {
+        result.data.url = `https://raw.githubusercontent.com/vega/vega-datasets/gh-pages/${layer.data.url}`
+      }
+      return result
+    })
+  }
+  return spec;
+}
+
+export function modifySpec (spec, type) {
+  spec = modifyDataSourceLocation(spec)
+
+  if (type === 'VEGA_LITE') {
+    spec = compile(spec).spec
+  }
+
+  return spec
+}
+
 
 export const fetchSpecLookups = async (options = API_DEFAULT_PAGGINATION_OPTIONS) => {
   const response = await axios.get('/specs', { params: options })
@@ -12,10 +46,21 @@ export const fetchSpec = async (options) => {
 }
 
 export const publishSpec = async ({spec, author, type, title}) => {
-  const response = await axios.post(`/specs`, {
-     spec,
-     title,
-     preview: '<svg></svg>'
+
+  spec = modifySpec(spec)
+
+  const view = new vega.View(vega.parse(spec), {
+    logLevel: vega.Warn,
+    renderer: 'none',
   });
+
+  let preview = await view.toSVG();
+
+  const response = await axios.post(`/specs`, {
+    spec,
+    title,
+    preview
+  });
+
   return response.data;
 }
--- packages/zorko-web-ui/src/reducer/specs.js
+++ packages/zorko-web-ui/src/reducer/specs.js
@@ -1,48 +1,28 @@
 import { DEFAULT_SPECS_STATE } from '../state'
 import { NEW_SPEC_WIZARD_PUBLISH_SUCCESS, SPEC_SET } from '../action'
-import { compile } from 'vega-lite';
-
+import { modifySpec } from '../api'
 const initialState = { ...DEFAULT_SPECS_STATE }
 
-function modifyDataSourceLocation(spec) {
-  if (spec.data && spec.data.url) {
-    let url = spec.data.url
-
-    spec.data.url = `https://raw.githubusercontent.com/vega/vega-datasets/gh-pages/${url}`
-  }
-
-  if (spec.layer && spec.layer.length > 0) {
-    spec.layer = spec.layer.map(layer => {
-      let result = { ...layer }
-      if (layer.data && layer.data.url) {
-        result.data.url = `https://raw.githubusercontent.com/vega/vega-datasets/gh-pages/${layer.data.url}`
-      }
-      return result
-    })
-  }
-  return spec;
-}
-
-function addSpec(state, specContent) {
-  // TODO: replace with server-side
-  const spec = modifyDataSourceLocation(specContent.spec)
+function addSpec(state, spec) {
   return {
     ...state,
     byId: {
       ...state.byId,
-      [specContent.id] : {
-        ...specContent,
-        spec: compile(spec).spec
+      [spec.id] : {
+        ...spec,
       }
     }
   }
 }
 
-
 export default function specReducer(state = initialState, action) {
   switch (action.type) {
     case SPEC_SET :
-      return addSpec(state, action.payload)
+      let specContent = action.payload
+      return addSpec(state, {
+        ...specContent,
+        spec: modifySpec(specContent.spec, 'VEGA_LITE')
+      })
     case NEW_SPEC_WIZARD_PUBLISH_SUCCESS :
       return addSpec(state, action.payload)
     default:
